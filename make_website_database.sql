1ebb68f667d907f99663fb63a454a3c4054cc44e
Calder White <calderwhite1@gmail.com>
2016-09-22 10:54:35 -0400

    Update make_website_database.sql

--- make_website_database.sql
+++ make_website_database.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS academic_sources(
   url TEXT,
   subject TEXT,
   main_website TEXT
-)
-INSERT INTO academic_source values("http://mrjtaylor.weebly.com/mpm-1dw---01-block-d.html","Mathematics","https://mrjtaylor.weebly.com") 
-INSERT INTO academic_source values("http://mrjtaylor.weebly.com/mpm-1dw---02-block-e.html","Mathematics","https://mrjtaylor.weebly.com")
+);
+INSERT INTO academic_sources values("http://mrjtaylor.weebly.com/mpm-1dw---01-block-d.html","Mathematics","https://mrjtaylor.weebly.com");
+INSERT INTO academic_sources values("http://mrjtaylor.weebly.com/mpm-1dw---02-block-e.html","Mathematics","https://mrjtaylor.weebly.com");
